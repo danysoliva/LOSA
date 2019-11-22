bdf3e821c339616cd158e0c310de4dc92c981f7e
Danys Oliva <danys.oliva@AQUAFEEDHN.COM>
2019-11-22 07:55:05 -0600

    confirmation

--- LOSA/RecepcionMP/rptReporteIngresoTarima.cs
+++ LOSA/RecepcionMP/rptReporteIngresoTarima.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.Collections;
 using System.ComponentModel;
 using DevExpress.XtraReports.UI;
+using LOSA.Clases;
 
 namespace LOSA.RecepcionMP
 {
@@ -12,6 +13,17 @@ namespace LOSA.RecepcionMP
         {
             InitializeComponent();
             Tarima tar1 = new Tarima();
+            if (tar1.RecuperarRegistro(idTarima, null))
+            {
+                //IdUnidadMedida = dr.GetInt32(1);
+                lblCantidad.Text = string.Format("{0:0.00}", tar1.Cantidad);
+                 NombreTarima = dr.GetString(2);
+                TipoTarimaDescripcion = dr.GetString(3);
+                Proveedor = dr.GetString(4);
+                LoteMP = dr.GetString(5);
+                Cantidad = dr.GetInt32(7);
+            }
+
         }
 
     }
