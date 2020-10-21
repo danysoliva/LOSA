c8361a3cb5ca03f9805012a98e834ab9eab3a4d5
Oscar Bonilla <oscar.bonilla@AQUAFEEDHN.COM>
2020-10-21 14:54:17 -0600

    Cambio en sp, solo el nombre

--- LOSA/TransaccionesMP/frmEntregaTarimaReq.cs
+++ LOSA/TransaccionesMP/frmEntregaTarimaReq.cs
@@ -229,7 +229,7 @@ namespace LOSA.TransaccionesMP
                         con.Open();
 
                         //SqlCommand cmd = new SqlCommand("sp_set_insert_salida_tarima_bodega_mp", con);
-                        SqlCommand cmd = new SqlCommand("[dbo].[sp_set_insert_salida_tarima_bodega_mp_v3]", con);
+                        SqlCommand cmd = new SqlCommand("[sp_set_insert_salida_tarima_bodega_mp_v3]", con);
                         cmd.CommandType = CommandType.StoredProcedure;
                         cmd.Parameters.AddWithValue("@idtarima", tarimaEncontrada.Id);
                         cmd.Parameters.AddWithValue("@id_usuario", usuarioLogueado.Id);
