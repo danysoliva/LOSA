5f44dd823cb51ab9a35f72ae15bc12fcd35a8a29
Roger Euceda <reuceda05@hotmail.com>
2021-09-14 07:09:14 -0600

    Ingresos de MP Reproceso.

---
+++ LOSA/Logistica/xfrmMP_Reproceso.cs
@@ -0,0 +1,260 @@
+﻿using ACS.Classes;
+using DevExpress.XtraEditors;
+using LOSA.Clases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LOSA.Logistica
+{
+    public partial class xfrmMP_Reproceso : DevExpress.XtraEditors.XtraForm
+    {
+        UserLogin usuarioLogueado;
+        private string ItemCode;
+
+        public xfrmMP_Reproceso(UserLogin pUse)
+        {
+            InitializeComponent();
+            usuarioLogueado = pUse;
+
+            CargarMP();
+            cargarDatosProveedor();
+            LoadPresentaciones();
+            LoadNumeroTransaccion();
+            LoadTipoTransaccion();
+        }
+
+        private void CargarMP()
+        {
+            try
+            {
+                DataOperations dp = new DataOperations();
+
+                using (SqlConnection cnx = new SqlConnection(dp.ConnectionStringLOSA))
+                {
+                    cnx.Open();
+                    dsLogistica2.MP_Reproceso.Clear();
+                    SqlDataAdapter da = new SqlDataAdapter("dbo.sp_get_MP_reproceso", cnx);
+
+                    da.Fill(dsLogistica2.MP_Reproceso);
+
+                    cnx.Close();
+                }
+            }
+            catch (Exception error)
+            {
+                CajaDialogo.Error(error.Message);
+            }
+        }
+
+        private void cargarDatosProveedor()
+        {
+            try
+            {
+
+                DataOperations dp = new DataOperations();
+                SqlConnection cn = new SqlConnection(dp.ConnectionStringLOSA);
+                string SQL = @"exec sp_get_providersv2";
+
+
+                dsLogistica2.Proveedores.Clear();
+                SqlDataAdapter adat = new SqlDataAdapter(SQL, cn);
+                adat.Fill(dsLogistica2.Proveedores);
+
+            }
+            catch (Exception ec)
+            {
+                MessageBox.Show(ec.Message);
+            }
+        }
+
+        private void LoadPresentaciones()
+        {
+            try
+            {
+                DataOperations dp = new DataOperations();
+                SqlConnection con = new SqlConnection(dp.ConnectionStringLOSA);
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("sp_get_presentaciones_activas", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter adat = new SqlDataAdapter(cmd);
+                dsRecepcionMPx.presentaciones.Clear();
+                adat.Fill(dsRecepcionMPx.presentaciones);
+                con.Close();
+            }
+            catch (Exception ec)
+            {
+                CajaDialogo.Error(ec.Message);
+            }
+        }
+
+        private void glueProveedor_EditValueChanged(object sender, EventArgs e)
+        {
+            txtCodigoProveedor.Text = glueProveedor.EditValue.ToString();
+        }
+
+        private void LoadNumeroTransaccion()
+        {
+            try
+            {
+                DataOperations dp = new DataOperations();
+                SqlConnection con = new SqlConnection(dp.ConnectionStringLOSA);
+                con.Open();
+                SqlCommand cmm2 = new SqlCommand("sp_generar_codigo_from_tables_id", con);
+                cmm2.CommandType = CommandType.StoredProcedure;
+                cmm2.Parameters.AddWithValue("@id", 2);
+                string num_ingreso = cmm2.ExecuteScalar().ToString();
+                txtNumIngreso.Text = num_ingreso;
+                con.Close();
+            }
+            catch (Exception ec)
+            {
+                CajaDialogo.Error(ec.Message);
+            }
+        }
+
+        private void btnAtras_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void LoadTipoTransaccion()
+        {
+            try
+            {
+                DataOperations dp = new DataOperations();
+                SqlConnection con = new SqlConnection(dp.ConnectionStringLOSA);
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("sp_get_tipo_transaccion_kardex", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter adat = new SqlDataAdapter(cmd);
+                dsRecepcionMPx.Tipo_transacciones_kardex.Clear();
+                adat.Fill(dsRecepcionMPx.Tipo_transacciones_kardex);
+                con.Close();
+            }
+            catch (Exception ec)
+            {
+                CajaDialogo.Error(ec.Message);
+            }
+        }
+
+        private void cmdGuardar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(slueMP.Text))
+            {
+                CajaDialogo.Error("Es necesario seleccionar la Materia Prima!");
+                return;
+            }
+
+
+            if (Convert.ToDecimal(txtCantidadT.Text) <= 0)
+            {
+                CajaDialogo.Error("No se puede registrar una tarima con cantidad de materia en cero (0)!");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(gridLookUpEditPresentacion.Text))
+            {
+                CajaDialogo.Error("Es necesario seleccionar la presentacion de la Materia Prima!");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtLote.Text))
+            {
+                CajaDialogo.Error("Es obligatorio llenar el lote para la tarima!");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(dtFechaVencimiento.Text))
+            {
+                CajaDialogo.Error("Es obligatorio llenar la fecha de vencimiento de la materia prima!");
+                return;
+            }
+
+
+            int cantTarimas = 0;
+            try
+            {
+                cantTarimas = Convert.ToInt32(txtCantidadTarimasTotal.Text);
+            }
+            catch
+            {
+            }
+
+            if (cantTarimas <= 0)
+            {
+                CajaDialogo.Error("La cantidad minima de tarimas debe ser uno(1)!");
+                return;
+            }
+
+            int CantGuardo = 0;
+            //ArrayList Lista = new ArrayList();
+            for (int i = 1; i <= cantTarimas; i++)
+            {
+                try
+                {
+                    DataOperations dp = new DataOperations();
+                    SqlConnection con = new SqlConnection(dp.ConnectionStringLOSA);
+                    con.Open();
+
+                    SqlCommand cmm = new SqlCommand("sp_generar_codigo_from_tables_id", con);
+                    cmm.CommandType = CommandType.StoredProcedure;
+                    cmm.Parameters.AddWithValue("@id", 1);
+                    string barcode = cmm.ExecuteScalar().ToString();
+
+                    SqlCommand cmd = new SqlCommand("sp_insert_new_tarima_sin_boleta_mp", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@itemcode", this.ItemCode);
+                    cmd.Parameters.AddWithValue("@id_proveedor", txtCodigoProveedor.Text);
+                    cmd.Parameters.AddWithValue("@fecha_ingreso", dtFechaIngreso.EditValue);
+                    cmd.Parameters.AddWithValue("@numero_transaccion", txtNumIngreso.Text);
+                    cmd.Parameters.AddWithValue("@fecha_vencimiento", dtFechaVencimiento.EditValue);
+                    cmd.Parameters.AddWithValue("@fecha_produccion_materia_prima", dtFechaProduccion.EditValue);
+                    cmd.Parameters.AddWithValue("@lote_materia_prima", txtLote.Text);
+                    cmd.Parameters.AddWithValue("@id_presentacion", gridLookUpEditPresentacion.EditValue);
+                    cmd.Parameters.AddWithValue("@id_usuario", usuarioLogueado.Id);
+                    cmd.Parameters.AddWithValue("@id_tipo_transaccion_kardex", glTipoTransaccion.EditValue);
+                    cmd.Parameters.AddWithValue("@codigo_barra", barcode);
+                    cmd.Parameters.AddWithValue("@cant", txtCantidadT.Text);
+                    cmd.Parameters.AddWithValue("@peso", txtPeso.Text);
+                    //Lista.Add(Convert.ToInt32(cmd.ExecuteScalar()));
+                    cmd.ExecuteScalar();
+                    CantGuardo++;
+                    con.Close();
+                    //CajaDialogo.InformationAuto();
+
+                }
+                catch (Exception ec)
+                {
+                    CajaDialogo.Error(ec.Message);
+                }
+            }
+
+
+
+
+            if (CantGuardo > 0)
+            {
+                CajaDialogo.Information("Datos guardados exitosamente!");
+                this.Close();
+            }
+        }
+
+        private void slueMP_EditValueChanged(object sender, EventArgs e)
+        {
+           
+                 //txtMP_Name.Text = slueMP.Text;
+                this.ItemCode = slueMP.EditValue.ToString();
+
+        }
+    }
+}
