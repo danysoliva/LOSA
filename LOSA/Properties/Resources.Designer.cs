7ff3e7ecea89924b2c6a2365ac0642af9264c1c2
danys.oliva <danys.oliva@aquafeedhn.com>
2021-11-17 20:56:42 -0600

    confirmation changes menu principal logistica

--- LOSA/Properties/Resources.Designer.cs
+++ LOSA/Properties/Resources.Designer.cs
@@ -143,6 +143,16 @@ namespace LOSA.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap boxPT32px {
+            get {
+                object obj = ResourceManager.GetObject("boxPT32px", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap cancel {
             get {
                 object obj = ResourceManager.GetObject("cancel", resourceCulture);
