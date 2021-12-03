abb17f51ab5166c28be6911ed239cafa764f0cb3
Roger Euceda <reuceda05@hotmail.com>
2021-12-03 13:46:01 -0600

    Update BIN activo

--- LOSA/Produccion/xfrmCheckActiveBin.cs
+++ LOSA/Produccion/xfrmCheckActiveBin.cs
@@ -25,7 +25,8 @@ namespace LOSA.Produccion
         {
             InitializeComponent();
              GetBinActivo();
-            UpdateActiveBin();
+            RotularTanquesMP();
+            //UpdateActiveBin();
         }
 
         private void GetBinActivo()
@@ -33,13 +34,27 @@ namespace LOSA.Produccion
             try
             {
             DataOperations dp = new DataOperations();
-                int id_tank = 0;
+                //int id_tank = 0;
+
+
+                pcTank1.BackColor = Color.Silver;
+                pcTank2.BackColor = Color.Silver;
+                pcTank3.BackColor = Color.Silver;
+                pcTank5.BackColor = Color.Silver;
+                pcTank4.BackColor = Color.Silver;
+                pcTank6.BackColor = Color.Silver;
+                pcTank7.BackColor = Color.Silver;
+                pcTank8.BackColor = Color.Silver;
+                pcTank9.BackColor = Color.Silver;
+                pcTank10.BackColor = Color.Silver;
+                pcTank11.BackColor = Color.Silver;
+                pcTank12.BackColor = Color.Silver;
 
                 using (SqlConnection cnx = new SqlConnection(dp.ConnectionStringAPMS))
                 {
                     cnx.Open();
 
-                    SqlCommand cmd = new SqlCommand("dbo.sp_get_bin_activoV2", cnx);
+                    SqlCommand cmd = new SqlCommand("dbo.sp_get_bin_activo", cnx);
                     cmd.CommandType = CommandType.StoredProcedure;
 
                     SqlDataReader dr = cmd.ExecuteReader();
@@ -47,13 +62,115 @@ namespace LOSA.Produccion
                     if (dr.Read())
                     {
                         id_active_tank = Convert.ToInt32(dr.GetInt32(0));
-                        label_tank = dr.GetString(1);
-                        mp = dr.GetString(2);
+                        //label_tank = dr.GetString(1);
+                        //mp = dr.GetString(2);
                     }
 
 
                     cnx.Close();
                 }
+
+
+                if (Convert.ToInt32(pcTank1.Tag.ToString()) == id_active_tank)
+                {
+                    pcTank1.BackColor = Color.Lime;
+                    lblBIN.Text = "FD"+ id_active_tank;
+                    //lblMP1.Visible = true;
+                    //lblMP1.Text = mp;
+                }
+
+                if (Convert.ToInt32(pcTank2.Tag.ToString()) == id_active_tank)
+                {
+                    pcTank2.BackColor = Color.Lime;
+                    lblBIN.Text =  "FD" + id_active_tank;
+                    //lblMP2.Visible = true;
+                    //lblMP2.Text = mp;
+                }
+
+                if (Convert.ToInt32(pcTank3.Tag.ToString()) == id_active_tank)
+                {
+                    pcTank3.BackColor = Color.Lime;
+                    lblBIN.Text = "FD" + id_active_tank;
+                    //lblMP3.Visible = true;
+                    //lblMP3.Text = mp;
+                }
+
+                if (Convert.ToInt32(pcTank4.Tag.ToString()) == id_active_tank)
+                {
+                    pcTank4.BackColor = Color.Lime;
+                    lblBIN.Text = "FD" + id_active_tank;
+                    //lblMP4.Visible = true;
+                    //lblMP4.Text = mp;
+                }
+
+                if (Convert.ToInt32(pcTank5.Tag.ToString()) == id_active_tank)
+                {
+                    pcTank5.BackColor = Color.Lime;
+                    lblBIN.Text = "FD" + id_active_tank;
+                    //lblMP5.Visible = true;
+                    //lblMP5.Text = mp;
+                }
+
+
+                if (Convert.ToInt32(pcTank6.Tag.ToString()) == id_active_tank)
+                {
+                    pcTank6.BackColor = Color.Lime;
+                    lblBIN.Text = "FD" + id_active_tank;
+                    //lblMP6.Visible = true;
+                    //lblMP6.Text = mp;
+                }
+                if (Convert.ToInt32(pcTank7.Tag.ToString()) == id_active_tank)
+                {
+                    pcTank7.BackColor = Color.Lime;
+                    lblBIN.Text = "FD" + id_active_tank;
+                    //lblMP7.Visible = true;
+                    //lblMP7.Text = mp;
+                }
+
+                if (Convert.ToInt32(pcTank8.Tag.ToString()) == id_active_tank)
+                {
+                    pcTank8.BackColor = Color.Lime;
+                    lblBIN.Text = "FD" + id_active_tank;
+                    //lblMP8.Visible = true;
+                    //lblMP8.Text = mp;
+                }
+
+                if (Convert.ToInt32(pcTank9.Tag.ToString()) == id_active_tank)
+                {
+                    pcTank9.BackColor = Color.Lime;
+                    lblBIN.Text = "FD" + id_active_tank;
+                    //lblMP9.Visible = true;
+                    //lblMP9.Text = mp;
+                }
+
+                if (Convert.ToInt32(pcTank10.Tag.ToString()) == id_active_tank)
+                {
+                    pcTank10.BackColor = Color.Lime;
+                    lblBIN.Text = "FD" + id_active_tank;
+                    //lblMP10.Visible = true;
+                    //lblMP10.Text = mp;
+                }
+
+                if (Convert.ToInt32(pcTank11.Tag.ToString()) == id_active_tank)
+                {
+                    pcTank11.BackColor = Color.Lime;
+                    lblBIN.Text = "FD" + id_active_tank;
+                    //lblMP11.Visible = true;
+                    //lblMP11.Text = mp;
+                }
+
+                if (Convert.ToInt32(pcTank12.Tag.ToString()) == id_active_tank)
+                {
+                    pcTank12.BackColor = Color.Lime;
+                    lblBIN.Text = "FD" + id_active_tank;
+                    //lblMP12.Visible = true;
+                    //lblMP12.Text = mp;
+                }
+
+
+
+
+
                 //return id_tank;
             }
             catch (Exception ex)
@@ -78,7 +195,7 @@ namespace LOSA.Produccion
         private void timer1_Tick(object sender, EventArgs e)
         {
             GetBinActivo();
-            UpdateActiveBin();
+            //UpdateActiveBin();
         }
 
         private void UpdateActiveBin()
@@ -204,6 +321,120 @@ namespace LOSA.Produccion
                 lblBIN.Text = label_tank;
                 lblMP12.Visible = true;
                 lblMP12.Text = mp;
+            }
+        }
+
+        private void RotularTanquesMP()
+        {
+            try
+            {
+                DataOperations dp = new DataOperations();
+                //int id_tank = 0;
+                 
+                int contador = 0;
+
+                using (SqlConnection cnx = new SqlConnection(dp.ConnectionStringAPMS))
+                {
+                    cnx.Open();
+
+                    SqlCommand cmd = new SqlCommand("dbo.sp_get_MP_by_tanque", cnx);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    while (dr.Read())
+                    {
+                        if ((contador+1)==1)
+                        {
+                            lblFD1.Text= dr.GetString(1);
+                            lblMP1.Text= dr.GetString(2);
+                        }
+
+                        if ((contador + 1) == 2)
+                        {
+                            lblFD2.Text = dr.GetString(1);
+                            lblMP2.Text = dr.GetString(2);
+                        }
+
+                        if ((contador + 1) == 3)
+                        {
+                            lblFD3.Text = dr.GetString(1);
+                            lblMP3.Text = dr.GetString(2);
+                        }
+
+                        if ((contador + 1) == 4)
+                        {
+                            lblFD4.Text = dr.GetString(1);
+                            lblMP4.Text = dr.GetString(2);
+                        }
+
+                        if ((contador + 1) == 5)
+                        {
+                            lblFD5.Text = dr.GetString(1);
+                            lblMP5.Text = dr.GetString(2);
+                        }
+
+                        if ((contador + 1) == 6)
+                        {
+                            lblFD6.Text = dr.GetString(1);
+                            lblMP6.Text = dr.GetString(2);
+                        }
+
+                        if ((contador + 1) == 7)
+                        {
+                            lblFD7.Text = dr.GetString(1);
+                            lblMP7.Text = dr.GetString(2);
+                        }
+
+                        if ((contador + 1) == 8)
+                        {
+                            lblFD8.Text = dr.GetString(1);
+                            lblMP8.Text = dr.GetString(2);
+                        }
+
+                        if ((contador + 1) == 9)
+                        {
+                            lblFD9.Text = dr.GetString(1);
+                            lblMP9.Text = dr.GetString(2);
+                        }
+
+                        if ((contador + 1) == 10)
+                        {
+                            lblFD10.Text = dr.GetString(1);
+                            lblMP10.Text = dr.GetString(2);
+                        }
+
+                        if ((contador + 1) == 11)
+                        {
+                            lblFD11.Text = dr.GetString(1);
+                            lblMP11.Text = dr.GetString(2);
+                        }
+
+                        if ((contador + 1) == 12)
+                        {
+                            lblFD12.Text = dr.GetString(1);
+                            lblMP12.Text = dr.GetString(2);
+                        }
+
+                        /* labelMP[contad*/
+                        //or].Text =  dr.GetString(2);
+                        contador++;
+                        //id_active_tank = Convert.ToInt32(dr.GetInt32(0));
+                        //label_tank = dr.GetString(1);
+                        //mp = dr.GetString(2);
+                    }
+
+
+
+
+                    cnx.Close();
+                }
+                //return id_tank;
+            }
+            catch (Exception ex)
+            {
+                CajaDialogo.Error(ex.Message);
+                //return 0;
             }
         }
     }
