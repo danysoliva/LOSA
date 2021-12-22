7818dc07df6035b2a7824741de8761f7c93b73f7
danys.oliva <danys.oliva@aquafeedhn.com>
2021-12-21 23:08:24 -0600

    confirmation changes

---
+++ LOSA/Trazabilidad/ReportesTRZ/frmTrazabilidadHaciaAdelanteByMP_Lot.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace LOSA.Trazabilidad.ReportesTRZ
+{
+    partial class frmTrazabilidadHaciaAdelanteByMP_Lot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTrazabilidadHaciaAdelanteByMP_Lot));
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.btnGenerar = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.txtlote = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.btnClearInfo = new DevExpress.XtraEditors.SimpleButton();
+            this.dsReportesTRZ1 = new LOSA.Trazabilidad.ReportesTRZ.dsReportesTRZ();
+            this.colLotePT = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colProducto = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridControl2 = new DevExpress.XtraGrid.GridControl();
+            this.gridView2 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtlote.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsReportesTRZ1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.gridControl1.DataMember = "pt_list_trz";
+            this.gridControl1.DataSource = this.dsReportesTRZ1;
+            this.gridControl1.Location = new System.Drawing.Point(2, 93);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(353, 484);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colLotePT,
+            this.colProducto});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Appearance.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.btnGenerar.Appearance.Options.UseFont = true;
+            this.btnGenerar.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnGenerar.ImageOptions.Image")));
+            this.btnGenerar.Location = new System.Drawing.Point(237, 46);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(118, 41);
+            this.btnGenerar.TabIndex = 42;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Location = new System.Drawing.Point(9, 61);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(61, 21);
+            this.labelControl1.TabIndex = 41;
+            this.labelControl1.Text = "Lote MP";
+            // 
+            // txtlote
+            // 
+            this.txtlote.Location = new System.Drawing.Point(76, 55);
+            this.txtlote.Name = "txtlote";
+            this.txtlote.Properties.Appearance.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtlote.Properties.Appearance.Options.UseFont = true;
+            this.txtlote.Properties.MaxLength = 4;
+            this.txtlote.Size = new System.Drawing.Size(155, 32);
+            this.txtlote.TabIndex = 40;
+            this.txtlote.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtlote_KeyDown);
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelControl3.Appearance.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl3.Appearance.Options.UseFont = true;
+            this.labelControl3.Appearance.Options.UseTextOptions = true;
+            this.labelControl3.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.labelControl3.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
+            this.labelControl3.Location = new System.Drawing.Point(224, 1);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(372, 39);
+            this.labelControl3.TabIndex = 39;
+            this.labelControl3.Text = "Trazabilidad de lote Materia Prima";
+            // 
+            // btnClearInfo
+            // 
+            this.btnClearInfo.Appearance.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClearInfo.Appearance.Options.UseFont = true;
+            this.btnClearInfo.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnClearInfo.ImageOptions.Image")));
+            this.btnClearInfo.Location = new System.Drawing.Point(361, 46);
+            this.btnClearInfo.Name = "btnClearInfo";
+            this.btnClearInfo.Size = new System.Drawing.Size(118, 41);
+            this.btnClearInfo.TabIndex = 43;
+            this.btnClearInfo.Text = "Borrar";
+            this.btnClearInfo.Click += new System.EventHandler(this.btnClearInfo_Click);
+            // 
+            // dsReportesTRZ1
+            // 
+            this.dsReportesTRZ1.DataSetName = "dsReportesTRZ";
+            this.dsReportesTRZ1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // colLotePT
+            // 
+            this.colLotePT.FieldName = "Lote PT";
+            this.colLotePT.Name = "colLotePT";
+            this.colLotePT.Visible = true;
+            this.colLotePT.VisibleIndex = 0;
+            this.colLotePT.Width = 140;
+            // 
+            // colProducto
+            // 
+            this.colProducto.FieldName = "Producto";
+            this.colProducto.Name = "colProducto";
+            this.colProducto.Visible = true;
+            this.colProducto.VisibleIndex = 1;
+            this.colProducto.Width = 481;
+            // 
+            // gridControl2
+            // 
+            this.gridControl2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridControl2.Location = new System.Drawing.Point(372, 142);
+            this.gridControl2.MainView = this.gridView2;
+            this.gridControl2.Name = "gridControl2";
+            this.gridControl2.Size = new System.Drawing.Size(428, 435);
+            this.gridControl2.TabIndex = 44;
+            this.gridControl2.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView2});
+            // 
+            // gridView2
+            // 
+            this.gridView2.GridControl = this.gridControl2;
+            this.gridView2.Name = "gridView2";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Segoe UI", 12.75F, System.Drawing.FontStyle.Bold);
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Appearance.Options.UseTextOptions = true;
+            this.labelControl2.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.labelControl2.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
+            this.labelControl2.Location = new System.Drawing.Point(402, 116);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(372, 24);
+            this.labelControl2.TabIndex = 45;
+            this.labelControl2.Text = "Clientes que recibieron el Producto ";
+            // 
+            // frmTrazabilidadHaciaAdelanteByMP_Lot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 579);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.gridControl2);
+            this.Controls.Add(this.btnClearInfo);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.txtlote);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.gridControl1);
+            this.Name = "frmTrazabilidadHaciaAdelanteByMP_Lot";
+            this.Text = "Trazabilidad desde Lote MP";
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtlote.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsReportesTRZ1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraEditors.SimpleButton btnGenerar;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.TextEdit txtlote;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.SimpleButton btnClearInfo;
+        private dsReportesTRZ dsReportesTRZ1;
+        private DevExpress.XtraGrid.Columns.GridColumn colLotePT;
+        private DevExpress.XtraGrid.Columns.GridColumn colProducto;
+        private DevExpress.XtraGrid.GridControl gridControl2;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView2;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+    }
+}
---
+++ LOSA/Trazabilidad/ReportesTRZ/frmTrazabilidadHaciaAdelanteByMP_Lot.cs
@@ -0,0 +1,68 @@
+﻿using ACS.Classes;
+using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LOSA.Trazabilidad.ReportesTRZ
+{
+    public partial class frmTrazabilidadHaciaAdelanteByMP_Lot : DevExpress.XtraEditors.XtraForm
+    {
+        public frmTrazabilidadHaciaAdelanteByMP_Lot(string pLoteMateriaPrima)
+        {
+            InitializeComponent();
+            txtlote.Text = pLoteMateriaPrima;
+            LoadLotesPT();
+        }
+
+        private void LoadLotesPT()
+        {
+            //[sp_load_lotes_pt_trz_from_lote_mp] @lotemp
+            try
+            {
+                DataOperations dp = new DataOperations();
+                SqlConnection con = new SqlConnection(dp.ConnectionStringLOSA);
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("sp_load_lotes_pt_trz_from_lote_mp", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@lotemp", txtlote.Text);
+                dsReportesTRZ1.pt_list_trz.Clear();
+                SqlDataAdapter adat = new SqlDataAdapter(cmd);
+                adat.Fill(dsReportesTRZ1.pt_list_trz);
+
+                con.Close();
+            }
+            catch (Exception ec)
+            {
+                CajaDialogo.Error(ec.Message);
+            }
+        }
+
+        private void txtlote_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                LoadLotesPT();
+            }
+        }
+
+        private void btnClearInfo_Click(object sender, EventArgs e)
+        {
+            txtlote.Text = "";
+            txtlote.Focus();
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            LoadLotesPT();
+        }
+    }
+}
