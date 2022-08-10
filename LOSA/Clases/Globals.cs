2f28c3cba33fa8eb334172c7e1a144573fc824d2
Danys Oliva <danys.oliva@AQUAFEEDHN.COM>
2022-08-10 12:57:21 -0600

    confirmation

--- LOSA/Clases/Globals.cs
+++ LOSA/Clases/Globals.cs
@@ -55,8 +55,8 @@ namespace ACS.Classes
         //LOSA
         public static string LOSA_ServerAddress = "AQFSVR003";
         public static string LOSA_ServerName = "AQFSVR003";
-        //public static string LOSA_ActiveDB = "LOSA"; //BASE PRODUCTIVA
-        public static string LOSA_ActiveDB = "LOSA2"; //BASE DE PRUEBAS
+        public static string LOSA_ActiveDB = "LOSA"; //BASE PRODUCTIVA
+        //public static string LOSA_ActiveDB = "LOSA2"; //BASE DE PRUEBAS
         public static string LOSA_DB_User = "sa";
         public static string LOSA_DB_Pass = " AquaF33dHN2014";
 
