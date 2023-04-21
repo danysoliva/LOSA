75ef2a5ff21dbd352e0b1a671add3a32a987568a
rubengarcia08 <48605282+rubengarcia08@users.noreply.github.com>
2023-04-20 23:27:12 -0600

    cambios

--- LOSA/Produccion/frmGenerarTarimas.cs
+++ LOSA/Produccion/frmGenerarTarimas.cs
@@ -438,7 +438,7 @@ namespace LOSA.Produccion
                     using (ReportPrintTool prinTool = new ReportPrintTool(report1))
                     {
                         report1.ShowPreviewMarginLines = false;
-                        prinTool.ShowPreview();
+                        prinTool.ShowPreviewDialog();
                         //prinTool.ShowPreview();
                     }
                 }
